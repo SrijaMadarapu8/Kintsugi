4dec67c6fc58c7177b3402a128c6150b2a43e069
SrijaMadarapu8 <srijam358@gmail.com>
2024-06-19 21:37:42 -0700

    move the avtar to right of the canvas

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 #README
+# yarn
+# yarn dev
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,27 @@
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+import { Vector3 } from "three";
 
 export const Experience = () => {
+  const { camera } = useThree();
+  
+  useEffect(() => {
+    camera.position.set(1, 0, 5);
+  }, [camera]);
+
   return (
     <>
-      <OrbitControls /> 
-      <group position-y={-1}>
-        <Avatar/>
+      <OrbitControls 
+        enablePan={false}
+        enableZoom={false}
+        enableRotate={false}
+      />
+      <group position={[2, -1, 0]}>
+        <Avatar />
       </group>
-      <ambientLight intensity={1}/>
+      <ambientLight intensity={1} />
     </>
   );
 };
